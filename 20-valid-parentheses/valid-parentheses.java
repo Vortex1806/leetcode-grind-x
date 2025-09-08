78da3fe14cce6946a43999306449196483f9132f
Shubh Vora <78277252+Vortex1806@users.noreply.github.com>
2025-09-08 23:30:28 +0530

    Time: 5 ms (6.67%) | Memory: 42.1 MB (15.40%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
+
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                st.push(s.charAt(i));
+            } else {
+                if(st.isEmpty()) {
+                    return false;
+                }
+                if(map.get(st.peek()) == s.charAt(i)) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if(st.isEmpty())return true;
+        return false;
+    }
+}
