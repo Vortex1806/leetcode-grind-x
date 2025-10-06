7cad3bb26c45a5273ae1b79f6c09e8dc2b2bb345
Shubh Vora <78277252+Vortex1806@users.noreply.github.com>
2025-10-06 22:45:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (31.17%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,13 +1,14 @@
 class Solution {
     public void sub(int[] nums, int ind, List<Integer> subList, List<List<Integer>> newList) {
-        if(ind >= nums.length) {
-            newList.add(subList);
+        if(ind == nums.length) {
+            newList.add(new ArrayList<>(subList));
             return;
         }
+
         subList.add(nums[ind]);
-        sub(nums, ind+1, new ArrayList<>(subList), newList);
-        subList.remove(new Integer(nums[ind]));
-        sub(nums, ind+1, new ArrayList<>(subList), newList);
+        sub(nums, ind + 1, subList, newList);
+        subList.remove(subList.size() - 1);
+        sub(nums, ind + 1, subList, newList);
     }
 
     public List<List<Integer>> subsets(int[] nums) {
