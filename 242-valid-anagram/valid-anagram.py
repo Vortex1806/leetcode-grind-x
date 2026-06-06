36c6acda98621ba6b3ca48c658325998bf92894d
Shubh Vora <78277252+Vortex1806@users.noreply.github.com>
2026-06-06 10:02:16 +0530

    Time: 15 ms (43.23%) | Memory: 19.4 MB (44.21%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,10 +3,9 @@ class Solution:
         if len(s) != len(t):
             return False
         freq = [0] * 26
-        for c in s:
-            freq[ord(c) - ord('a')] += 1
-        for c in t:
-            freq[ord(c) - ord('a')] -= 1
+        for i in range(len(s)):
+            freq[ord(s[i]) - ord('a')] += 1
+            freq[ord(t[i]) - ord('a')] -= 1
         for val in freq:
             if val != 0:
                 return False
